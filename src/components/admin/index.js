592c298bc42ac57c0de8da75481dbe5d18e38466
lgonzalez <luis@ezgmz.com>
2018-10-23 19:01:36 -0400

    add select with data of back

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,72 +1,63 @@
 import React, { Component} from 'react';
-import { getAllRolesList, getAllUbicationsList } from '../connect_api/user/userAPI'
+import { getAllRolesList, getAllUbicationsList } from '../connect_api/user/userAPI';
+import Select from 'react-select';
 
 class admin extends Component {
 
-    constructor(){
-    super();
+    constructor(props){
+    super(props);
     this.state = {
       nombre: "",
       apellido: "",
       email: "",
-      rol: [],
-      ubicacion: [],
+      rol : "",
+      rolList: [],
+      ubicacion: "",
+      ubicacionList: [],
+      statusList:[],
       status: ""
     }
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
  componentDidMount() {
   getAllRolesList()
   .then(result => {
     this.setState({
-      rol: result
+      rolList: result
     })
-    console.log(this.state.rol[0].ROL);
+    console.log(this.state.rolList);
   });
   getAllUbicationsList()
   .then(result => {
     this.setState({
-      ubicacion : result
+      ubicacionList : result
     });
     console.log(this.state);
   })
  }
 
- updateNombre(event) {
-    this.setState({
-      nombre: event.target.value
-    });
-  }
+ handleChange = event => {
+   this.setState({
+     [event.target.id]: event.target.value
+   });
+   console.log(this.state)
+ }
+ handleChangeSelectrol = event => {
+   this.setState({
+     rol : event.value
+   });
+   console.log(this.state)
+ }
 
- updateApellido(event) {
-    this.setState({
-      apellido: event.target.value
-    });
-  }
-
- updateEmail(event) {
-    this.setState({
-      email: event.target.value
-    });
-  }
-
-   updateRol(event) {
-    this.setState({
-      rol: event.target.value
-    });
-  }
-
-   updateUbicacion(event) {
-    this.setState({
-      ubicacion: event.target.value
-    });
-  }
-
-   updateStatus(event) {
-    this.setState({
-      status: event.target.value
-    });
-  }
+ handleChangeSelectub = event => {
+   this.setState({
+     ubicacion : event.value
+   });
+   console.log(event.value)
+   console.log(this.state)
+ }
 
   render() {
     return (
@@ -75,26 +66,40 @@ class admin extends Component {
 
         <div className="form-group">
             <label htmlFor="nombre"> Nombre</label>
-            <input className="form-control" type="text" name="nombre" id="nombre" value={this.state.nombre} onChange={this.updateNombre.bind(this)}/>
+            <input className="form-control" type="text" name="nombre" id="nombre" value={this.state.nombre} onChange={this.handleChange}/>
       </div>
 
       <div className="form-group">
             <label htmlFor="apellido"> Apellido</label>
-            <input className="form-control" type="text" name="apellido" id="apellido" value={this.state.apellido} onChange={this.updateApellido.bind(this)}/>
+            <input className="form-control" type="text" name="apellido" id="apellido" value={this.state.apellido} onChange={this.handleChange}/>
       </div>
 
       <div className="form-group">
         <label htmlFor="email"> Email</label>
-            <input className="form-control" type="text" name="email" id="email" value={this.state.email} onChange={this.updateEmail.bind(this)}/>
+            <input className="form-control" type="text" name="email" id="email" value={this.state.email} onChange={this.handleChange}/>
       </div>
 
       <div className="form-group">
             <label htmlFor="rol"> Rol</label>
+            <Select
+              id = "rol"
+              name = "rol"
+              onChange={this.handleChangeSelectrol}
+              options={this.state.rolList.map(rol =>(
+              {label: rol.ROL, value : rol.ID}
+            ))}
+            />
       </div>
 
       <div className="form-group">
         <label htmlFor="ubicacion"> Ubicación</label>
-            <input className="form-control" type="text" name="ubicacion" id="ubicacion" value={this.state.ubicacion} onChange={this.updateUbicacion.bind(this)}/>
+        <Select
+        value={this.state.ubicacion}
+        onChange={this.handleChangeSelectub}
+        options={this.state.ubicacionList.map(ub =>(
+          {label: ub.Ubicacion, value : ub.ID}
+        ))}
+        />
       </div>
 
       <div className="form-group">
