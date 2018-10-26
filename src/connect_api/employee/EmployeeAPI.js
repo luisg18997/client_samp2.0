c3821ec867a485920807032fa9524c3964306d80
luis <luisgonzalez18997@gmail.com>
2018-10-26 00:36:42 -0400

    connect back with the views

---
+++ src/connect_api/employee/EmployeeAPI.js
@@ -0,0 +1,20 @@
+const api = process.env.URL_API || 'http://localhost:5000/employee/';
+
+
+
+
+export const getAllStatesList = () => fetch(`${api}states`, 
+	{ method: 'GET', headers: { 'Content-Type': 'application/json' } })
+	.then(res => res.json())
+	.then(states => states.map(states =>({
+		ID : states.id,
+		name : states.state
+	})))
+	.catch((error) => {
+    	console.log('The error is:', error.message);
+  	});
+
+  	export const postMovPer = (data) => {
+	const result = data;
+	console.log('result: ', result);
+}
