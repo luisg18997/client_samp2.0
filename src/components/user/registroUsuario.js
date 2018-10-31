af34bf9782046ace7bf40228a652fccce690cab2
lgonzalez <luis@ezgmz.com>
2018-10-31 19:03:16 -0400

    update

--- src/components/user/registroUsuario.js
+++ src/components/user/registroUsuario.js
@@ -29,7 +29,6 @@ class RegistroUsuario extends Component {
     this.setState({
       [event.target.id]: event.target.value
     });
-		console.log(this.state);
   }
 
 	componentDidMount() {
@@ -46,48 +45,45 @@ class RegistroUsuario extends Component {
     event.preventDefault();
 		addNewUser(this.state)
 		.then(result => {
-			if(result === 0) {
-				alert('usuario ya existente');
-				this.props.history.push('/Registro');
-			} else {
+			if(result === 1) {
 				alert('usuario creado exitosamente');
 				this.props.history.push('/');
+			} else {
+				alert('usuario ya existente');
+				this.props.history.push('/Registro');
 			}
 		});
-		this.setState({
-				nombre: null,
-				apellido : null,
-				email: null,
-				clave: null,
-				ubicacion: null
-			})
   }
 	render(){
+		const { selectedOptionUb } = this.state.ubicacion
+		const { nombre } = this.state.nombre;
 		return(
 			<div className="container">
 			<form onSubmit={this.handleSubmit} className="form-container">
 
 		        <div className="form-group">
 		            <label htmlFor="nombre"> Nombre</label>
-		            <input className="form-control" type="text" name="nombre" id="nombre" value={this.state.nombre} onChange={this.handleChange}/>
+		            <input className="form-control" type="text" name="nombre" id="nombre" value={nombre} onChange={this.handleChange} required/>
 		      </div>
 
 		      <div className="form-group">
 		            <label htmlFor="apellido"> Apellido</label>
-		            <input className="form-control" type="text" name="apellido" id="apellido" value={this.state.apellido} onChange={this.handleChange} />
+		            <input className="form-control" type="text" name="apellido" id="apellido" value={this.state.apellido} onChange={this.handleChange} required/>
 		      </div>
 
 		      <div className="form-group">
 		        <label htmlFor="email"> Email</label>
-		            <input className="form-control" type="email" name="email" id="email" value={this.state.email} onChange={this.handleChange}/>
+		            <input className="form-control" type="email" name="email" id="email" value={this.state.email} onChange={this.handleChange} required/>
 		      </div>
 		      <div className="form-group">
 		        <label htmlFor="clave"> Clave</label>
-		            <input className="form-control" type="password" name="clave" id="clave" value={this.state.clave} onChange={this.handleChange}/>
+		            <input className="form-control" type="password" name="clave" id="clave" value={this.state.clave} onChange={this.handleChange} required/>
 		      </div>
 		      <div className="form-group">
        			 <label htmlFor="ubicacion"> Ubicación</label>
 					<Select
+							required
+							value={selectedOptionUb}
 			        onChange={this.handleChangeSelectub}
 			        options={this.state.ubicacionList.map(ub =>(
 			          {label: ub.Ubicacion, value : ub.ID}
