9885d4e7ed419ab7d3ec53fb4fa333ebb8f71e08
denis osuna <denisosuna@gmail.com>
2018-11-10 12:53:22 +0000

    Upload New File

---
+++ src/components/budget/ListadoPlanillas.js
@@ -0,0 +1,18 @@
+import React, { Component} from 'react';
+
+
+class mainBudget extends Component {
+	  
+
+	render(){
+		return(
+
+			<div>
+
+			<p> Contenido Principal de Presupuesto</p>
+			</div>
+		)
+	}
+}
+
+export default mainBudget;
