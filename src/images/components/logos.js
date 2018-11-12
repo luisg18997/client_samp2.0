961711e40765ea267c660a0fa321239c8b578c7e
lgonzalez <luis@ezgmz.com>
2018-11-12 19:01:09 -0400

    correction in the header

--- src/images/components/logos.js
+++ src/images/components/logos.js
@@ -5,8 +5,8 @@ import logo2 from '../faculta_humanidades_educacion.png';
 export class UCV extends Component {
   render() {
     return (
-      <div className="text-left" style={{ marginLeft: 10 }}>
-        <img src={logo} style = {{width: "30%", height: "40%"}} alt= "ucv" />
+      <div className="text-left" style={{ marginLeft: 10}}>
+        <img src={logo} className="logo_ucv" alt= "ucv" />
       </div>
     )
   }
@@ -16,7 +16,7 @@ export class FHE extends Component {
   render() {
     return (
       <div className = "text-right" style={{ marginRight: 10 }}>
-        <img src={logo2} style= {{width: "60", height: '60', padding : '0'}} alt = "fhe"/>
+        <img src={logo2} className="logo_fhe" alt = "fhe"/>
       </div>
     )
   }
