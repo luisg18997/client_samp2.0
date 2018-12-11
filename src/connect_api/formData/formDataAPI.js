bc2c35afbdcc6ce075f7a110958b995181430675
luis <luisgonzalez18997@gmail.com>
2018-12-11 12:27:18 -0400

    update call of api

--- src/connect_api/formData/formDataAPI.js
+++ src/connect_api/formData/formDataAPI.js
@@ -1,65 +1,74 @@
 const api = process.env.URL_API || 'http://localhost:5000/form/';
 
 export const getAllMovementTypeslist = () => fetch(`${api}MovementTypes`,
-	{ method: 'GET', headers: { 'Content-Type': 'application/json' } })
-	.then(res => res.json())
-	.then(MovementTypes => MovementTypes.map(MovementTypes =>({
-		ID : MovementTypes.id,
+{
+	method: 'GET',
+	headers: { 
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	}
+	
+})
+.then(res => res.json())
+.then(MovementTypes => MovementTypes.map(MovementTypes =>({
+	ID : MovementTypes.id,
+	name : MovementTypes.description
+})))
+.catch((error) => {
+	console.log('The error is:', error.message);
+});
 
-		name : MovementTypes.description
-	})))
-	.catch((error) => {
-    	console.log('The error is:', error.message);
-  	});
+export const addNewFormOfice = (employee, ofice, userID, employeeId) => fetch(`${api}ofice/addOfice`,
+{
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	},
+	body : JSON.stringify({
+		param_employee : employee,
+		param_form_ofice : ofice,
+		param_user_id : userID,
+		param_employee_id : employeeId
+	})
+})
+.then(res => res.json())
+.catch((error) => {
+	console.log('The error is:', error.message);
+});
 
-	export const addNewFormOfice = (employee, ofice, userID, employeeId) => fetch(`${api}ofice/addOfice`,
-		{
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body : JSON.stringify({
-				param_employee : employee,
-				param_form_ofice : ofice,
-				param_user_id : userID,
-				param_employee_id : employeeId
-			})
-		})
-		.then(res => res.json())
-		.catch((error) => {
-	    	console.log('The error is:', error.message);
-	  	});
+export const CodeOfice = (schoolID, instituteID, coordinationID) => fetch(`${api}ofice/CodeOfice`,
+{
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	},
+	body : JSON.stringify({
+		param_school_id : schoolID,
+		param_institute_id : instituteID,
+		param_coordination_id : coordinationID
+	})
+})
+.then(res => res.json())
+.catch((error) => {
+	console.log('The error is:', error.message);
+});
 
-			export const CodeOfice = (schoolID, instituteID, coordinationID) => fetch(`${api}ofice/CodeOfice`,
-				{
-					method: 'post',
-					headers: {
-						'Content-Type': 'application/json'
-					},
-					body : JSON.stringify({
-						param_school_id : schoolID,
-						param_institute_id : instituteID,
-						param_coordination_id : coordinationID
-					})
-				})
-				.then(res => res.json())
-				.catch((error) => {
-			    	console.log('The error is:', error.message);
-			  	});
-
-					export const codeMovPer = (schoolID, instituteID, coordinationID) => fetch(`${api}MovPersonal/CodeMovPer`,
-						{
-							method: 'post',
-							headers: {
-								'Content-Type': 'application/json'
-							},
-							body : JSON.stringify({
-								param_school_id : schoolID,
-								param_institute_id : instituteID,
-								param_coordination_id : coordinationID
-							})
-						})
-						.then(res => res.json())
-						.catch((error) => {
-					    	console.log('The error is:', error.message);
-					  	});
+export const codeMovPer = (schoolID, instituteID, coordinationID) => fetch(`${api}MovPersonal/CodeMovPer`,
+{
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+		'Accept': 'application/json'
+	},
+	body : JSON.stringify({
+		param_school_id : schoolID,
+		param_institute_id : instituteID,
+		param_coordination_id : coordinationID
+	})
+})
+.then(res => res.json())
+.catch((error) => {
+	console.log('The error is:', error.message);
+});
