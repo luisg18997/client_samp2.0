c0d59aef7cc573b8e65ed3d92a07acbca2f51ee3
luis <luisgonzalez18997@gmail.com>
2018-12-28 00:23:17 -0400

    add table in the role of budget

--- src/components/budget/ListadoPlanillas.js
+++ src/components/budget/ListadoPlanillas.js
@@ -1,20 +1,97 @@
 import React, { Component} from 'react';
+import { MDBDataTable } from 'mdbreact';
+import { MDBBtn } from 'mdbreact';
+import {
+	getFormsList
+}
+from '../../connect_api/formData/formDataAPI'
 
+class ListPlanillas extends Component {
+	  constructor(){
+			super();
+			this.state={
+				table:{
+					columns:[
+						{
+							label:"Codigo de Planilla",
+							field: "code_form",
+	            width: 250
+						},
+						{
+							label:"Tipo de planilla",
+							 width: 250
+						},
+						{
+							label:"Tipo de Movimiento",
+							width: 350
+						},
+						{
+							label:"Ubicacion",
+							width: 750
+						},
+						{
+							label: "Fecha de Registro",
+							sort: 'asc',
+							width: 350
+						},
+						{
+							label: "Status",
+							width: 250
+						},
+						{
+							label: "Acción",
+							width: 250
+						}
+					]
+				},
+				isLoaded: false
+			}
+		}
 
-class mainBudget extends Component {
-	  
+		 componentWillMount(){
+			getFormsList(6,0)
+			.then(result =>{
+				console.log('getFormsList: ',result);
+				const { table } = this.state;
+				table.rows = result.map(form => ({
+					codigo : form.code_form,
+					tipo : form.form_type,
+					movement_type : form.movement_type,
+					ubication : form.ubication,
+					registration_date : form.registration_date,
+					status_form : form.status_form,
+					button : <MDBBtn onClick={(e) => this.handleData(e,form.identification, form.form_type)} >Seleccionar</MDBBtn>
+				}));
+				this.setState({
+					table,
+					isLoaded : true
+				})
+				console.log('rows: ', this.state)
+			})
+		}
+		handleData = (e, identification, formType) => {
+			e.preventDefault();
+	    console.log("ListPlanillas: ",identification," ", formType);
+		}
 
 	render(){
+		if (!this.state.isLoaded) {
+			return (<div className="loader"></div>);
+		} else {
+
 		return(
 
-			<div style={{'padding':'10px','margin-top':'50px', 'margin-left':'65px',color:'#595959'}} class="content">
-
-
-     <h1 align="center">Listado de Solicitudes</h1>
-      <hr></hr>
+			<div style={{'padding':'10px',marginTop:'50px', marginLleft:'65px',color:'#595959'}} className="content">
+				<MDBDataTable
+					striped
+					hover
+					small
+					data={this.state.table}
+				/>
 			</div>
 		)
 	}
+	}
 }
 
-export default mainBudget;
+export default ListPlanillas;
