5e6df2efc02211715496756192379105573cb09b
luis <luisgonzalez18997@gmail.com>
2019-01-13 02:06:29 -0400

    add screen of check mov personal no finish

---
+++ src/components/human_Resources/movPersonalRev.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  getFormOfficial
+}
+  from '../../connect_api/formData/formDataAPI';
+import { MDBBtn } from 'mdbreact';
+import {
+	updateAllColumnsProcessMovPersonalForm
+}
+from '../../connect_api/processForm/processFormAPI'
+
+class movPersonalRev extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+}
+
+
+export default movPersonalRev;
