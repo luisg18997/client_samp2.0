75257602e416570d7f899595ff6021690bf9e8d1
luis <luisgonzalez18997@gmail.com>
2019-03-24 22:27:07 -0400

    add template of  PDF for mov personal form

--- src/components/school/movPersonalPDF.js
+++ src/components/school/movPersonalPDF.js
@@ -4,6 +4,7 @@ import {
   getFormMovPersonal
 }
   from '../../connect_api/formData/formDataAPI';
+import {UCV2, FHE } from '../../images/components/logos'
 import Authorization from '../redirectPrincipal';
 
 class movPersonalPDF extends Component {
@@ -125,16 +126,147 @@ class movPersonalPDF extends Component {
         <div id="MovPersonal" style={{
           visibility: "hidden"
         }}>
-        <table align="center" width="100">
-	 <tr align="center">
-        <td width="100" align="letf"></td>
-        <td width="320"><b>UNIVERSIDAD CENTRAL DE VENEZUELA</b><br /><br />
-        	<b>FACULTAD DE HUMANIDADES Y EDUCACION</b></td>
-        <td width="250"><b>Solicitud de Movimiento de Personal</b><br/><br/>
-        	Numero:<strong>{this.state.codigo}</strong></td>
-        <td width="110"></td>
-    </tr>
-</table>
+          <table align="center" width="100">
+  	         <tr align="center">
+               <td width="100" align="letf"><UCV2 /></td>
+               <td width="320"><b>UNIVERSIDAD CENTRAL DE VENEZUELA</b><br /><br />
+          	     <b>FACULTAD DE HUMANIDADES Y EDUCACION</b></td>
+               <td width="250"><b>Solicitud de Movimiento de Personal</b><br/><br/>
+          	     Numero:<strong>{this.state.codigo}</strong></td>
+               <td width="110"><FHE /></td>
+            </tr>
+          </table>
+          <table border="3">
+            <tr>
+                <td colSpan='3'>
+                  <b>Escuela: {this.state.escuela}</b>
+                </td>
+                <td>
+                  <b>Fecha:</b> {this.state.fecha_reg}
+                </td>
+            </tr>
+            <tr>
+                <td colSpan='2'>
+                  <b>departamento:</b> {this.state.departamento}
+                  <br/>
+                  <br/>
+                  <b>Nombres y Apellidos:</b> {this.state.nombre} {this.state.snombre} {this.state.apellido} {this.state.sapellido}
+                </td>
+                <td colSpan='2'>
+                  <b>Cátedra:</b> {this.state.catedra}
+                    <br/>
+                    <br/>
+                  <b>C.I.:</b> {this.state.cedula}
+                </td>
+            </tr>
+            <tr align="center">
+                <td colSpan='1.5'>
+                  <b>Tipo de Movimiento</b>
+                </td>
+                <td>
+                  <b>Dedicación Actual</b>
+                </td>
+                <td>
+                  <b>Dedicación Propuesta</b>
+                </td>
+            </tr>
+            <tr align="center">
+                <td colSpan='1.5'>
+                  {this.state.tip_mov.description}
+                </td>
+                <td>
+                  {this.state.dedicacion}
+                </td>
+                <td>
+                  {this.state.dedicacionPro}
+                </td>
+            </tr>
+            <tr align="center">
+                <td>
+                  <b>Sueldo</b>
+                </td>
+                <td>
+                  <b>Unidad Ejecutora</b>
+                </td>
+                <td>
+                  <b>Lapso</b>
+                </td>
+                <td>
+                  <b>Categoría</b>
+                </td>
+            </tr>
+            <tr align="center">
+                <td>
+                  {this.state.sueldo}
+                </td>
+                <td>
+                  {this.state.unidad_ejec}
+                </td>
+                <td>
+                  Inicio: {this.state.fecha_ini}
+                  <br/>
+                  Fin: {this.state.fecha_fin}
+                </td>
+                <td>
+                  {this.state.categoria}
+                </td>
+            </tr>
+            <tr align="center">
+                <td colSpan='4'>
+                  <b>Justificación u Observaciones: </b> {this.state.observacion}
+                  <br />
+                  <b>Dirección:</b> {this.state.direccion}
+                  <br />
+                  <b>Ingreso:</b> {this.state.ingreso} <b>Tipo Ingreso:</b> {this.state.ingresoType} <b>Fecha Ingreso:</b> {this.state.ingresoDate}
+                  <br />
+                  <b>IDAC: {this.state.idac} </b>
+                </td>
+            </tr>
+            <tr>
+                <td colSpan='4'>
+                  <b>Anexos:</b> {this.state.anexo}
+                </td>
+            </tr>
+            <tr>
+              <td colSpan='2'>
+                <br/>
+          			<br/>
+          			<br/>
+          			<br/>
+          			<br/>
+			          _______________________________<br/>
+			           <b>Director o Jefe de Dependencia</b>
+              </td>
+              <td colSpan='2'>
+                <br/>
+          			<br/>
+          			<br/>
+          			<br/>
+          			<br/>
+			          _______________________________<br/>
+			           <b>Decano o Coordinador</b>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan='2' width="50%">
+                <b>Unidad Ejecutora:</b> {this.state.unidad_ejec}
+                <br/>
+                <b>Cógo Programa:</b>
+                <br/>
+                <b>Código Contable:</b>
+                <br/>
+                <b>Sueldo:</b> {this.state.sueldo}
+                <br/>
+                <b>Fecha Efectiva:</b>
+                <br/>
+                <br/>
+                <b>Firma del Jefe de Presupuesto</b>
+              </td>
+              <td colSpan='2' align="center" valign="top">
+                <b>Observaciones de Departamento de Presupuesto</b>
+              </td>
+            </tr>
+          </table>
 
         </div>
       </Fragment>
